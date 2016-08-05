0adb50502a3a716873579a35f6bd948a8e49808b
jiehua <jiehua@ebay.com>
2016-08-05 12:11:01 +0800

    first vue

--- src/entry.js
+++ src/entry.js
@@ -19,3 +19,14 @@ require("./style/style.css");
 
 //Part6. 加载图片 url-loader 没成功
 
+//Part7. 一个vue文件就是一个单独的组件，vue组件开发是具有高度的独立性并且易于维护的。组件的划分可大可小，一个页面可以看做多个vue组件构成的。
+// vue-loader vue文件组件化， vue-router 用来做路由分发的(http://vuejs.github.io/vue-router/zh-cn/index.html)
+
+//Part8. vue
+var Vue = require("vue");
+var app = new Vue({
+    el:"#Vue1",
+    data:{
+        message:"Hello Vue.js"
+    }
+})
